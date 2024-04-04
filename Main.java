54407ca4c2e05d395525473b59641144ee391720
Vaishnavi Dere <116423327+Vaish208@users.noreply.github.com>
2024-04-04 14:05:35 +0530

    PreDefined inputs - Merge two arrays
    Q2. Merge two arrays by satisfying given constraints - Predefined Inputs

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Given inputs in example 
+        int[] vacantCells = {0, 2, 0, 3, 0, 5, 6, 0, 0}; 
+        int[] elementsToAdd = {1, 8, 9, 10, 15};
+
+        int count = 0;
+        for (int i = 0; i < vacantCells.length; i++) {
+            if (vacantCells[i] != 0) {
+                count++;
+            }
+        }
+
+        int[] mergedArray = new int[count + elementsToAdd.length];
+
+        int index = 0;
+        for (int i = 0; i < vacantCells.length; i++) {
+            if (vacantCells[i] != 0) {
+                mergedArray[index++] = vacantCells[i];
+            }
+        }
+
+
+        for (int i = 0; i < elementsToAdd.length; i++) {
+            mergedArray[index++] = elementsToAdd[i];
+        }
+
+        
+        Arrays.sort(mergedArray);           // Sort the mergedArray
+
+        System.out.println(Arrays.toString(mergedArray));
+    }
+}
